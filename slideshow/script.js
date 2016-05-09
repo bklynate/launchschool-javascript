c0cf105911dd217d02275ff4677acefc1c050d68
Nathan Perkins <nperkins487@gmail.com>
2016-05-09 03:12:26 +0000

    slideshow done

--- slideshow/script.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  $("#preview img").click(function(e) {
-    var $new_img = $(this).clone().hide();
-    $("#zoomed img").fadeOut();
-    $new_img.appendTo("#zoomed").fadeIn();
-  });
-});
