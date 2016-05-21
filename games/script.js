8afb648b0abdda06b0aa0a8dd61c64cccb466fc8
Nathan Perkins <nperkins487@gmail.com>
2016-05-21 01:24:19 +0000

    filtering project, content done

---
+++ games/script.js
@@ -0,0 +1,58 @@
+var catalog = [{
+    "title": "The Legend of Zelda: Majora's Mask 3D",
+    "id": 1,
+    "category": "Nintendo 3DS"
+  }, {
+    "title": "Super Smash Bros.",
+    "id": 2,
+    "category": "Nintendo 3DS"
+  }, {
+    "title": "Super Smash Bros.",
+    "id": 3,
+    "category": "Nintendo WiiU"
+  }, {
+    "title": "LEGO Batman 3: Beyond Gotham",
+    "id": 4,
+    "category": "Nintendo WiiU"
+  }, {
+    "title": "LEGO Batman 3: Beyond Gotham",
+    "id": 5,
+    "category": "Xbox One"
+  }, {
+    "title": "LEGO Batman 3: Beyond Gotham",
+    "id": 6,
+    "category": "PlayStation 4"
+  }, {
+    "title": "Far Cry 4",
+    "id": 7,
+    "category": "PlayStation 4"
+  }, {
+    "title": "Far Cry 4",
+    "id": 8,
+    "category": "Xbox One"
+  }, {
+    "title": "Call of Duty: Advanced Warfare",
+    "id": 9,
+    "category": "PlayStation 4"
+  }, {
+    "title": "Call of Duty: Advanced Warfare",
+    "id": 10,
+    "category": "Xbox One"
+  }];
+
+$(function addListItems() {
+  $ul = $("#content ul");
+
+  catalog.forEach(function(item) {
+    $ul.append('<li data-id="' + item.id + '">' + item.title + ' for ' + item.category);
+  });
+});
+
+$(function addCheckBoxHandler() {
+  $("input[type=checkbox]").change(function(e) {
+    e.preventDefault();
+    var category = $(this).val();
+
+    $("#content ")
+  });
+});
